5701227db2d6c764967215c585fde45bbef54160
Ivan Kwong <ivankwongtszfung@gmail.com>
2020-06-08 01:43:31 +0800

    set a right setup.py

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ setup(
     name="HelloWorld",
     version="0.1",
     packages=find_packages(),
-    package_data={'': ['*.txt']},
+    data-files=[('.'::['text.txt'])],
     include_package_data=True,
     use_scm_version = {
         "root": "..",
